01516207c6cbcde56ce104c0d17c9299f29b66fe
gurudiel <gurudiel@users.noreply.github.com>
2016-04-12 23:54:52 -0400

    ProyectoFinal_

---
+++ Game/cocos2d.js
@@ -0,0 +1,20 @@
+(function () {
+    var d = document;
+    var c = {
+        COCOS2D_DEBUG: 2, 
+        box2d: true,
+        showFPS: false,
+        frameRate: 60,
+        tag: 'viewport',
+        SingleEnginFile: '../cocos2d/cocos2dx.min.js',
+        
+        appFiles: ['resources.js', 'b2.js', 'app.js']
+    };
+    window.addEventListener('DOMContentLoaded', function () {
+        var s = d.createElement('script');
+        s.src = c.SingleEnginFile;
+        d.body.appendChild(s);
+        s.c = c;
+        s.id = 'cocos2d-html5';
+    });
+})();
